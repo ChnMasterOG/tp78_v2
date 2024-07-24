5c2af04c373c447599bc2a365d9eed411adcc5f3
ChnMasterOG <670476161@qq.com>
2024-07-24 20:59:35 +0800

    Fix RF project build fail
    replace GCC8 to GCC12

--- mounriver_project/RF_CH582M/obj/StdPeriphDriver/subdir.mk
+++ mounriver_project/RF_CH582M/obj/StdPeriphDriver/subdir.mk
@@ -85,6 +85,6 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 StdPeriphDriver/%.o: ../StdPeriphDriver/%.c
-	@	@	riscv-none-embed-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -msmall-data-limit=8 -mno-save-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\Startup" -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\Ld" -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\RVMSIS" -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\StdPeriphDriver\inc" -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\APP\include" -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\HAL\include" -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\LIB" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@	@	riscv-none-elf-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -msmall-data-limit=8 -mno-save-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\Startup" -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\Ld" -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\RVMSIS" -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\StdPeriphDriver\inc" -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\APP\include" -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\HAL\include" -I"E:\Project\tp78_v2\mounriver_project\RF_CH582M\LIB" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
 	@	@
 
